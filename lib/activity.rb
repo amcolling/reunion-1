1859bfaaa5625433c81cd0c17e76ae8ec959891e
amcolling <amcolling@gmail.com>
2018-05-30 09:38:33 -0600

    initial commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,20 @@
+require 'pry'
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(attributes)
+    @activity_name = attributes[:activity_name]
+    @base_cost = attributes[:base_cost]
+    @cost_per_participant = attributes[:cost_per_participant]
+    @participants = attributes[:participants]
+  end
+
+  def add_participants(customers)
+    customers 
+  end
+
+
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new({activity_name: 'noodling', base_cost: 10, cost_per_participant: 20, participants:{name: 'Angi',cost: '20'} })
+    assert_instance_of Activity, activity
+  end
+
+  def test_participants
+    activity = Activity.new({activity_name: 'noodling', base_cost: 10, cost_per_participant: 20, participants:{name: 'Angi',cost: '20'} })
+    assert_equal ({:name=>"Angi", :cost=>"20"}), activity.participants
+  end
+
+  def test_add_participants
+    activity = Activity.new({activity_name: 'noodling', base_cost: 10, cost_per_participant: 20, participants:{name: 'Angi',cost: '20'} })
+    expected = activity.add_participants({name: 'Angi',cost: '20'})
+    assert_equal ({:name=>"Angi", :cost=>"20"}), expected
+  end
+
+
+
 end
